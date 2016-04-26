121c083094cc7e6b25f38c50d398345829af8a8f
Vincil Bishop <vincil.bishop@vbishop.com>
2016-04-26 18:01:16 -0500

    Updated for custom strong blocks

--- Pod/Classes/Blocks/BIGBlocks.h
+++ Pod/Classes/Blocks/BIGBlocks.h
@@ -17,3 +17,12 @@ typedef void (^BIGBlock)(id sender, id result);
 typedef void (^BIGCompletionBlock)(id sender, BOOL success, NSError *error, id result);
 
 typedef void (^BIGStateChangeBlock)(id sender, id oldState, id newState);
+
+#define BIG_STRONG_COMPLETION(__RESULT_TYPE__) \
+void (^)(id sender, BOOL success, NSError *error, __RESULT_TYPE__ result)
+
+#define BIG_STRONG_SENDER_COMPLETION(__SENDER_TYPE__,__RESULT_TYPE__) \
+void (^)(__SENDER_TYPE__ sender, BOOL success, NSError *error, __RESULT_TYPE__ result)
+
+#define BIG_STRONG_RETURN(__INPUT_TYPE__,__RETURN_TYPE__) \
+__RETURN_TYPE__ (^)(__INPUT_TYPE__ input)
