280c718d571631fd3808f3530efb010083f68a46
Vincil Bishop <vincil.bishop@vbishop.com>
2016-05-12 18:18:48 -0500

    Added big data

--- Pod/Classes/Categories/BIGCategories.h
+++ Pod/Classes/Categories/BIGCategories.h
@@ -13,6 +13,7 @@
 #import "NSDateFormatter+RailsJSONDate.h"
 #import "NSDecimalNumber+BIGCurrencyHelpers.h"
 #import "NSNumber+BIGCurrencyHelpers.h"
+#import "NSString+BIGDataHelpers.h"
 #import "NSString+BIGJSONHelpers.h"
 #import "NSString+BIGValidationHelpers.h"
 
